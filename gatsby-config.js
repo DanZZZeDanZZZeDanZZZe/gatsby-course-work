701b51ce9f1d0adcbfb4a4595f759b794090ccb5
Artem Grankin <artem_grankin@epam.com>
2020-10-16 17:07:35 +0300

    chore: add yaml plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,21 @@
 module.exports = {
   plugins: [
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
