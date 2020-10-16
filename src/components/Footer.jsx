919803e0f15ccb976ec2fc052f07eb5e9211542a
Artem Grankin <artem_grankin@epam.com>
2020-10-16 17:08:43 +0300

    feature: add data and query for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
 
 import '../styles/footer.css'
 
 export default function Footer({ modifier }) {
+  const data = useStaticQuery(graphql`
+    query FooterQuery {
+      file(base: {eq: "footer.yaml"}) {
+        childDataYaml {
+          author
+          linkAddress
+          linkText
+        }
+      }
+    }
+  `)
+  const {author, linkAddress, linkText} = data.file.childDataYaml
+
   return (
     <footer  className={`footer ${modifier}`}>
-      <span className="author">Выполни: Гранкин А.Н.</span>
-      <a className="link" href="https://vk.com/esli_prochital_eto_tu_molodech">Страница VK</a>
+      <span className="author">{author}</span>
+      <a className="link" href={linkAddress}>{linkText}</a>
     </footer>
   )
 }
